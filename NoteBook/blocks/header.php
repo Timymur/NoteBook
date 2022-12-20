f9e02ea908b9ef6e63b629da65cbfe17d10b3a14
Timymur <105367869+Timymur@users.noreply.github.com>
2022-12-20 14:12:31 +0500

    testinfo

--- NoteBook/blocks/header.php
+++ NoteBook/blocks/header.php
@@ -4,6 +4,7 @@
     <a href="main.php" class="kn">Показать все</a>
     <a href="add_note.php" class="kn">Добавить</a>
     <a href="find.php" class="kn">Найти</a>
+    <a href="test.php" class="kn">test.php</a>
 
 
   </nav>
